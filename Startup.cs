b69342d8e65c1461bf29662b22aaa8c36e3de59f
Codeguruu <namananilgoyal@gmail.com>
2026-02-23 12:27:26 +0530

    feat: Add web application with TradeController and views for trade risk analysis.

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using TradeNexus.Web.Data;
+using TradeNexus.Web.Services;
 
-public void ConfigureServices(IServiceCollection services)
+namespace TradeNexus.Web
 {
-    services.AddControllersWithViews();
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-    services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Register PythonRiskService for DI
+            services.AddScoped<PythonRiskService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Trade}/{action=Index}/{id?}");
+            });
+        }
+    }
 }
--- TradeNexus.Web/Controllers/TradeController.cs
+++ TradeNexus.Web/Controllers/TradeController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using System.Linq;
+using System.Threading.Tasks;
 using TradeNexus.Web.Data;
 using TradeNexus.Web.Services;
 
@@ -9,31 +11,39 @@ namespace TradeNexus.Web.Controllers
     public class TradeController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly PythonRiskService _riskService;
 
-        public TradeController(ApplicationDbContext context)
+        public TradeController(ApplicationDbContext context, PythonRiskService riskService)
         {
             _context = context;
+            _riskService = riskService;
         }
 
-        public IActionResult CalculateRisk(int clientId)
+        public async Task<IActionResult> Index()
         {
-            var trades = _context.Trades
+            var clients = await _context.Clients.Include(c => c.SubBroker).ToListAsync();
+            return View(clients);
+        }
+
+        public async Task<IActionResult> CalculateRisk(int clientId)
+        {
+            var trades = await _context.Trades
                 .Where(t => t.ClientId == clientId)
                 .Select(t => new { quantity = t.Quantity, price = t.Price })
-                .ToList();
+                .ToListAsync();
 
             var input = new
             {
                 trades = trades,
-                availableMargin = 500000
+                availableMargin = 500000 // In production, this would come from a ClientMargin table
             };
 
             string jsonInput = JsonConvert.SerializeObject(input);
 
-            var service = new PythonRiskService();
-            var result = service.ExecuteRiskEngine(jsonInput);
+            var result = _riskService.ExecuteRiskEngine(jsonInput);
 
             ViewBag.RiskResult = result;
+            ViewBag.ClientId = clientId;
 
             return View("RiskResult");
         }
